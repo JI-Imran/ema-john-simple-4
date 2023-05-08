cf8c6e0df50286452a68ded41c304356c9564aa2
JI-Imran <jiimran525@gmail.com>
2023-05-09 00:27:16 +0600

    nameawsedfasdft

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,6 +9,10 @@ const Cart = ({cart}) => {
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
+            <p>Total Price:</p>
+            <p>Total Shipping:</p>
+            <p>Tax:</p>
+            <h6>Grand Total: {}</h6>
         </div>
     );
 };
